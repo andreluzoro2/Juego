ccdf67a4ed8a81f73b73aa748f477182a9a82c71
andreluzoro2 <31522279+andreluzoro2@users.noreply.github.com>
2023-05-14 18:36:41 -0500

    Add files via upload

---
+++ adit.js
@@ -0,0 +1,87 @@
+
+function myFunction() {
+    document.getElementById("im1").classList.add("tra");
+    document.getElementById("im2").classList.add("tra");
+    document.getElementById("im3").classList.add("tra");
+    document.getElementById("im4").classList.add("tra");
+    document.getElementById("im5").classList.add("tra");
+    document.getElementById("im6").classList.add("tra");
+    document.getElementById("im7").classList.add("tra");
+    document.getElementById("im8").classList.add("tra");
+    document.getElementById("im9").classList.add("tra");
+    
+}
+function myFunction2() {
+    document.getElementById("im1").classList.remove("tra");
+    document.getElementById("im2").classList.remove("tra");
+    document.getElementById("im3").classList.remove("tra");
+    document.getElementById("im4").classList.remove("tra");
+    document.getElementById("im5").classList.remove("tra");
+    document.getElementById("im6").classList.remove("tra");
+    document.getElementById("im7").classList.remove("tra");
+    document.getElementById("im8").classList.remove("tra");
+    document.getElementById("im9").classList.remove("tra");
+    
+}
+ 
+
+ function F1(){
+    var leo = document.getElementById("im1");
+    leo.classList.add("tra");
+    var ti=1000*document.getElementById("level").value;
+    setTimeout(()=>{leo.classList.remove("tra")},ti);
+}
+
+function F2(){
+    var leo = document.getElementById("im2");
+    leo.classList.add("tra");
+    var ti=1000*document.getElementById("level").value;
+    setTimeout(()=>{leo.classList.remove("tra")},ti);
+}
+function F3(){
+    var leo = document.getElementById("im3");
+    leo.classList.add("tra");
+    var ti=1000*document.getElementById("level").value;
+    setTimeout(()=>{leo.classList.remove("tra")},ti);
+}
+function F4(){
+    var leo = document.getElementById("im4");
+    leo.classList.add("tra");
+    var ti=1000*document.getElementById("level").value;
+    setTimeout(()=>{leo.classList.remove("tra")},ti);
+}
+function F5(){
+    var leo = document.getElementById("im5");
+    leo.classList.add("tra");
+    var ti=1000*document.getElementById("level").value;
+    setTimeout(()=>{leo.classList.remove("tra")},ti);
+}
+function F6(){
+    var leo = document.getElementById("im6");
+    leo.classList.add("tra");
+    var ti=1000*document.getElementById("level").value;
+    setTimeout(()=>{leo.classList.remove("tra")},ti);
+}
+function F7(){
+    var leo = document.getElementById("im7");
+    leo.classList.add("tra");
+    var ti=1000*document.getElementById("level").value;
+    setTimeout(()=>{leo.classList.remove("tra")},ti);
+}
+function F8(){
+    var leo = document.getElementById("im8");
+    leo.classList.add("tra");
+    var ti=1000*document.getElementById("level").value;
+    setTimeout(()=>{leo.classList.remove("tra")},ti);
+}
+function F9(){
+    var leo = document.getElementById("im9");
+    leo.classList.add("tra");
+    var ti=1000*document.getElementById("level").value;
+    setTimeout(()=>{leo.classList.remove("tra")},ti);
+}
+
+function niv(){
+    var nive=document.getElementById("level").value;
+
+}
